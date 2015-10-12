27b18ff90035505327bcc12fe8727100f0fbdeac
jmvilaverde <jmvilaverde@gmail.com>
2015-10-12 13:40:24 +0200

    Checkpoint 20151012

--- 1.Get_Data.R
+++ 1.Get_Data.R
@@ -1,4 +1,5 @@
 
+getData <- function(){
 #Set URL Data path
 URLData <<- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/yelp_dataset_challenge_academic_dataset.zip"
 
@@ -8,16 +9,19 @@ directoryRaw <<- "data/raw"
 #Create directory, if it doesn't exists
 if (!dir.exists(directoryRaw)) dir.create(directoryRaw, recursive=TRUE)
 
-fileZip <<- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset.zip", sep = "/", collapse = NULL)
+fileZip <- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset.zip", sep = "/", collapse = NULL)
 
 if (!file.exists(fileZip))
         {
                 download.file(URLData, fileZip)
                 unzip(fileZip, exdir = directoryRaw)
         }
+}
 
+getJSON <- function(){
 #Get data from file
-library(rjson)
+# library(rjson)
+library(jsonlite)
 basePath <- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_", sep = "/")
 
 json_file_business <- paste(basePath, "business.json", sep ="")
@@ -25,23 +29,32 @@ json_file_business <- paste(basePath, "business.json", sep ="")
 
 #CHECKPOINT!! 20151007
 if (file.exists(json_file_business))
-        json_data_business <- fromJSON(paste(readLines(json_file_business), collapse=","))
+        json_data_business <<- fromJSON(paste("[",paste(readLines(json_file_business), collapse=","),"]"), simplifyDataFrame = TRUE)
         #json_data_business <- fromJSON(json_file_business)
 
+business <- with(json_data_business,cbind(business_id, full_address, open, as.character(city), review_count, name,
+                                          neighborhoods, longitude, as.character(state), latitude, as.character(type))) 
+business <-  as.data.frame(business, stringsAsFactors = FALSE)
+colnames(business) <- c("business_id", "full_address", "open", "city", "review_count", "name",
+                        "neighborhoods", "longitude", "state", "latitude", "type")
+
+business_categories <- with(json_data_business,cbind(business_id, as.character(categories))) 
+business_categories <-  as.data.frame(business_categories, stringsAsFactors = FALSE)
+colnames(business_categories) <- c("business_id", "categories")
+
+business_hours <- with(json_data_business,cbind(business_id, hours)) 
+business_hours <-  as.data.frame(business_hours, stringsAsFactors = FALSE)
+
+business_attributes <- with(json_data_business,cbind(business_id, as.character(attributes))) 
+business_attributes <-  as.data.frame(business_attributes, stringsAsFactors = FALSE)
+colnames(business_attributes) <- c("business_id", "attributes")
+
+
+
+}
 
 main <- function(){
         getData()
-        readData(directoryRaw)
+        getJSON()
         
-}
-#Code from
-#https://www.safaribooksonline.com/blog/2014/01/13/reading-and-parsing-external-data-in-r/
-
-df <- data.frame()
-
-for(n in json_data_business){
-        business_id <- n$business_id
-        full_address <- n$full_address
-        newrow <- data.frame(business_id, full_address, stringsAsFactors=FALSE, check.rows=FALSE)
-        df <- rbind(df, newrow)
 }
