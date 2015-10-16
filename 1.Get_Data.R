85359e603e5b4981e88a7bed70eaf5d111ac9282
Jose Maria Vilaverde <jmvilaverde@gmail.com>
2015-10-16 14:53:18 +0200

    Checkpoint 20151015

--- 1.Get_Data.R
+++ 1.Get_Data.R
@@ -1,47 +1,57 @@
-
-#Set URL Data path
-URLData <<- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/yelp_dataset_challenge_academic_dataset.zip"
-
-#Set directory where it's to be stored
-directoryRaw <<- "data/raw"
-
-#Create directory, if it doesn't exists
-if (!dir.exists(directoryRaw)) dir.create(directoryRaw, recursive=TRUE)
-
-fileZip <<- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset.zip", sep = "/", collapse = NULL)
-
-if (!file.exists(fileZip))
+getData <- function(){
+        #Set URL Data path
+        URLData <<- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/yelp_dataset_challenge_academic_dataset.zip"
+        
+        #Set directory where it's to be stored
+        directoryRaw <<- "data/raw"
+        
+        #Create directory, if it doesn't exists
+        if (!dir.exists(directoryRaw)) dir.create(directoryRaw, recursive=TRUE)
+        
+        fileZip <- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset.zip", sep = "/", collapse = NULL)
+        
+        if (!file.exists(fileZip))
         {
                 download.file(URLData, fileZip)
                 unzip(fileZip, exdir = directoryRaw)
         }
+}
 
-#Get data from file
-library(rjson)
-basePath <- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_", sep = "/")
-
-json_file_business <- paste(basePath, "business.json", sep ="")
-
-
-#CHECKPOINT!! 20151007
-if (file.exists(json_file_business))
-        json_data_business <- fromJSON(paste(readLines(json_file_business), collapse=","))
-        #json_data_business <- fromJSON(json_file_business)
-
+getJSON <- function(){
+        #Get data from file
+        # library(rjson)
+        library(jsonlite)
+        require(dplyr)
+        
+        basePath <- paste(directoryRaw, "yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_", sep = "/")
+        
+        files <- c("business", "checkin", "review", "tip", "user")
+        json_file <- list()
+        json_data <<- list()
+        
+        
+        limit = 100
+        
+        for (set in files) {
+                json_file[set] <- paste(basePath, set, ".json", sep ="")
+                
+                print(json_file[set])
+                if (file.exists(as.character(json_file[set]))){
+                        data <- fromJSON(paste("[",paste(readLines(as.character(json_file[set]), n=limit), 
+                                                         collapse=","),"]"), simplifyDataFrame = TRUE)
+                        # Forum solution to  flatten file
+                        # https://class.coursera.org/dsscapstone-005/forum/thread?thread_id=24
+                        json_data[[set]] <<- flatten(data)
+                }
+                
+        }
+        
+        #json_data
+        
+}
 
 main <- function(){
         getData()
-        readData(directoryRaw)
+        getJSON()
         
-}
-#Code from
-#https://www.safaribooksonline.com/blog/2014/01/13/reading-and-parsing-external-data-in-r/
-
-df <- data.frame()
-
-for(n in json_data_business){
-        business_id <- n$business_id
-        full_address <- n$full_address
-        newrow <- data.frame(business_id, full_address, stringsAsFactors=FALSE, check.rows=FALSE)
-        df <- rbind(df, newrow)
 }
